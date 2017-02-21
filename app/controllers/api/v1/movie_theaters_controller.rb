e411395484b33dd8828e2bdef99a5db753fe7033
Phellipe Lins <phellipelins@gmail.com>
2017-02-21 17:10:36 -0300

    new structure GET /api/v1/movie_theaters/:id.json

--- app/controllers/api/v1/movie_theaters_controller.rb
+++ app/controllers/api/v1/movie_theaters_controller.rb
@@ -76,9 +76,6 @@ module Api
         def parse_movie_theater(movie_theater_id)
           html = Nokogiri::HTML(open("http://www.cinepolis.com.br/programacao/cinema.php?cc=#{movie_theater_id}"))
 
-          # GET MOVIE DATA:
-          # http://www.cinepolis.com.br/programacao/busca.php?cidade=16&cc=16&cf=8344
-
           movie_theater = {
             name: html.css('.titulo .amarelo')[0].text,
             location: html.css('.titulo .cinza .esquerda')[0].text,
@@ -102,7 +99,7 @@ module Api
 
             if option.attr('value') != '0'
 
-              html = Nokogiri::HTML(open("http://www.cinepolis.com.br/programacao/busca.php?cidade=#{movie_theater_id}&cc=16&cf=#{option["value"]}"))
+              html = Nokogiri::HTML(open("http://www.cinepolis.com.br/programacao/busca.php?cidade=#{movie_theater_id}&cc=#{movie_theater_id}&cf=#{option['value']}"))
               trailer = Nokogiri::HTML(open(html.css('.linha2 .coluna1 a')[1].attr('href')))
 
               movie = {
@@ -120,15 +117,51 @@ module Api
               remaining_days = []
 
               html.css('.tabs3 .tabNavigation li').each do |li|
-                
+
                 day = {
-                  date: 'abc'
+                  date: li.css('span b').text,
+                  textual_day: li.css('span').text.gsub(/\d\d\/\d\d/, ''),
+                  sessions: []
                 }
                 
+                sessions = []
+
+                html.css("#{li.css('a')[0].attr('href')} table tr").each do |tr|
+                  
+                  if tr.attr('bgcolor') == '#990000'
+                    
+                    session = {
+                      room: tr.css('td')[2].text,
+                      subtitle: tr.css('td span[aria-label]')[0].attr('aria-label'),
+                      schedules: []
+                    }
+
+                    schedules = []
+
+                    tr.css('.horarios .hint--top').each_with_index do |link, index|
+                      if index != 0
+                        schedules << link.text
+                      end
+
+                    end
+
+                    session[:schedules] = schedules
+
+                    sessions << session
+
+                  end
+                end
+                
+                day[:sessions] = sessions
+
                 remaining_days << day
+
               end
 
+              movie[:remaining_days] = remaining_days
+
               movies << movie
+
             end
           end
 
